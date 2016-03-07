38e4ab7a440aef8a06d6ab72624a9430dc7c623d
MacMini2 <djsucho@gmail.com>
2016-03-07 14:33:40 +0100

    remove Article class

--- presenters/ArticlesPresenter.php
+++ presenters/ArticlesPresenter.php
@@ -4,13 +4,9 @@ namespace App\ArticleModule\Presenters;
 
 class ArticlesPresenter extends \App\Presenters\BasePresenter
 {
-	/** @var \App\ArticleModule\Model\Article @inject */
-	public $article;
-	
 	public function renderDefault()
 	{
 		$this->template->anyVariable = 'any value';
-		$this->template->time = $this->article->getTime();
 	}
 
 }
