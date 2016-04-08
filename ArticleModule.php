e5881c114734ba2a5307e0db40850b89ffb2f4bf
MacMini2 <djsucho@gmail.com>
2016-04-08 17:03:08 +0200

    Article

--- ArticleModule.php
+++ ArticleModule.php
@@ -3,11 +3,30 @@
 namespace Wame;
 
 use Wame\Core\Models\Plugin;
+use Wame\PermissionModule\Models\PermissionObject;
 
 class ArticleModule extends Plugin 
 {
-	public function onEnable() {
+	/** @var PermissionObject */
+	private $permission;
 
+	public function __construct(PermissionObject $permission) 
+	{
+		$this->permission = $permission;
 	}
+	
+	public function onEnable() 
+	{
+		$this->permission->addResource('article');
 
+		$this->permission->addResourceAction('article', 'view');
+		$this->permission->allow('guest', 'article', 'view');
+		$this->permission->addResourceAction('article', 'add');
+		$this->permission->allow('moderator', 'article', 'add');
+		$this->permission->addResourceAction('article', 'edit');
+		$this->permission->allow('moderator', 'article', 'edit');
+		$this->permission->addResourceAction('article', 'delete');
+		$this->permission->allow('moderator', 'article', 'delete');
+	}
+	
 }
