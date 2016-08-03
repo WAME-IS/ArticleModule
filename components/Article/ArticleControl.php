48e81a8e0523a9bc75997435a27b2b62a5761317
Linzee <d.gmiterko@gmail.com>
2016-08-03 12:55:01 +0200

    Creating Article example

--- components/Article/ArticleControl.php
+++ components/Article/ArticleControl.php
@@ -3,133 +3,31 @@
 namespace Wame\ArticleModule\Components;
 
 use Nette\DI\Container;
+use Nette\DI\Container;
 use Wame\ArticleModule\Entities\ArticleEntity;
-use Wame\ArticleModule\Repositories\ArticleRepository;
 use Wame\Core\Components\BaseControl;
+use Wame\ListControl\Components\IEntityControlFactory;
 
-interface IArticleControlFactory
+interface IArticleControlFactory extends IEntityControlFactory
 {
+
     /** @return ArticleControl */
-    public function create();
+    public function create($entity);
 }
 
 class ArticleControl extends BaseControl
 {
-    /** @var integer */
-    protected $id;
 
-    /** @var string */
-    protected $slug;
-
-    /** @var boolean */
-    private $inList = false;
-
-    /** @var ArticleEntity */
-    protected $articleEntity;
-
-    /** @var ArticleRepository */
-    private $articleRepository;
-
-    /** @var ArticleEntity */
     private $article;
 
-    /** @var string */
-    private $lang;
-    
-
-    public function __construct(Container $container, ArticleRepository $articleRepository)
+    public function __construct(Container $container, ArticleEntity $entity)
     {
         parent::__construct($container);
-
-        $this->articleRepository = $articleRepository;
-        $this->lang = $this->articleRepository->lang;
-
-//        $this->getPresenter()->getStatus()->set('meta', 'test');
+        $this->article = $entity;
     }
 
-
-    /**
-     * Render
-     * 
-     * @param ArticleEntity $articleEntity	article
-     */
-    public function render($articleEntity)
+    public function render()
     {
-        if ($articleEntity) {
-            $this->article = $articleEntity;
-            $this->getStatus()->set('article', $this->article);
-        } else {
-            $this->article = $this->getStatus()->get('article');
-//            if ($this->id) {
-//                $this->article = $this->getArticleById($this->id);
-//            } else if ($this->slug) {
-//                $this->article = $this->getArticleBySlug($this->slug);
-//            }
-        }
-
         $this->template->article = $this->article;
     }
-    
-    
-    /** methods ***************************************************************/
-
-    /**
-     * Set id
-     * 
-     * @param type $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * Set slug
-     * 
-     * @param type $slug
-     */
-    public function setSlug($slug)
-    {
-        $this->slug = $slug;
-    }
-
-    /**
-     * Set if in list
-     * 
-     * @param boolean $inList
-     */
-    public function setInList($inList)
-    {
-        $this->inList = $inList;
-    }
-    
-    
-    /**
-     * Get article by ID
-     * 
-     * @param integer $id		ID
-     * @return ArticleEntity	article
-     */
-    private function getArticleById($id)
-    {
-        return $this->articleRepository->get([
-                'id' => $id,
-                'status' => ArticleRepository::STATUS_PUBLISHED
-        ]);
-    }
-
-    /**
-     * Get article by slug
-     * 
-     * @param string $slug		slug
-     * @return ArticleEntity	article
-     */
-    private function getArticleBySlug($slug)
-    {
-        return $this->articleRepository->get([
-                'langs.slug' => $slug,
-                'status' => ArticleRepository::STATUS_PUBLISHED
-        ]);
-    }
-    
 }
--- components/ArticleList/ArticleListControl.php
+++ components/ArticleList/ArticleListControl.php
@@ -3,7 +3,12 @@
 namespace Wame\ArticleModule\Components;
 
 use Nette\ComponentModel\IContainer;
-use Nette\DI\Container;
+use Wame\ArticleModule\Components\ArticleListControl;
+use Wame\ArticleModule\Entities\ArticleEntity;
+use Wame\ChameleonComponents\Definition\ControlDataDefinition;
+use Wame\ChameleonComponents\Definition\DataDefinition;
+use Wame\ChameleonComponents\Definition\DataDefinitionTarget;
+use Wame\ChameleonComponents\IO\DataLoaderControl;
 use Wame\ChameleonComponentsDoctrineListControl\Provider\ChameleonComponentsListProvider;
 use Wame\ListControl\Components\ProvidedListControl;
 
@@ -14,12 +19,19 @@ interface IArticleListControlFactory
     public function create();
 }
 
-class ArticleListControl extends ProvidedListControl
+class ArticleListControl extends ProvidedListControl implements DataLoaderControl
 {
 
     public function __construct(Container $container, IContainer $parent = NULL, $name = NULL)
     {
         parent::__construct($container, $parent, $name);
         $this->setProvider(new ChameleonComponentsListProvider());
+        $this->setComponentFactory($componentFactory);
+        $this->setNoItemsFactory($noItemsFactory)
+    }
+
+    public function getDataDefinition()
+    {
+        return new ControlDataDefinition($this, new DataDefinition(new DataDefinitionTarget(ArticleEntity::class, true)));
     }
 }
