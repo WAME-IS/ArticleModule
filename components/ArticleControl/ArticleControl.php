d21db9553ea801e88341b1e44030168d395dad04
Linzee <d.gmiterko@gmail.com>
2016-08-28 19:02:19 +0200

    ADD tree renderers

--- components/ArticleControl/ArticleControl.php
+++ components/ArticleControl/ArticleControl.php
@@ -2,15 +2,8 @@
 
 namespace Wame\ArticleModule\Components;
 
-use Doctrine\Common\Collections\Criteria;
-use Nette\Application\BadRequestException;
-use Nette\DI\Container;
 use Wame\ArticleModule\Entities\ArticleEntity;
-use Wame\ChameleonComponents\Definition\ControlDataDefinition;
-use Wame\ChameleonComponents\Definition\DataDefinition;
-use Wame\ChameleonComponents\Definition\DataDefinitionTarget;
-use Wame\ChameleonComponents\IO\DataLoaderControl;
-use Wame\Core\Components\BaseControl;
+use Wame\Core\Components\SingleEntityControl;
 use Wame\ListControl\Components\IEntityControlFactory;
 
 interface IArticleControlFactory extends IEntityControlFactory
@@ -20,51 +13,11 @@ interface IArticleControlFactory extends IEntityControlFactory
     public function create($entity = null);
 }
 
-class ArticleControl extends BaseControl implements DataLoaderControl
+class ArticleControl extends SingleEntityControl
 {
 
-    /** @var int */
-    private $articleId;
-
-    /** @var ArticleEntity */
-    private $article;
-
-    public function __construct(Container $container, $entity = null)
+    protected function getEntityType()
     {
-        parent::__construct($container);
-
-        $this->getStatus()->get(ArticleEntity::class, function($article) {
-            if (!$article) {
-                throw new BadRequestException("Article with this id doesn't exist");
-            }
-            $this->article = $article;
-        });
-
-        if ($entity) {
-            $this->getStatus()->set(ArticleEntity::class, $entity);
-        }
-    }
-
-    /**
-     * @param int $articleId
-     */
-    public function setArticleId($articleId)
-    {
-        $this->articleId = $articleId;
-    }
-
-    public function getDataDefinition()
-    {
-        $criteria = null;
-        if ($this->articleId) {
-            $criteria = Criteria::create()->where(Criteria::expr()->eq('id', $this->articleId));
-        }
-
-        return new ControlDataDefinition($this, new DataDefinition(new DataDefinitionTarget(ArticleEntity::class, false), $criteria));
-    }
-
-    public function render()
-    {
-        $this->template->article = $this->article;
+        return ArticleEntity::class;
     }
 }
