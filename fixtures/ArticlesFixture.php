8776cc98a35494e64ea065717a60c1af4111a8b3
shadd0w <rene.gmitter@wame.sk>
2016-09-27 09:25:23 +0200

    MOVE fixture command to core

--- fixtures/ArticlesFixture.php
+++ fixtures/ArticlesFixture.php
@@ -17,9 +17,7 @@ class ArticlesFixture extends \Doctrine\Common\DataFixtures\AbstractFixture
 
     public function load(ObjectManager $manager)
     {
-//        var_dump($this->articleRepository->get(['id' => 1]));
         $user = $manager->getRepository(UserEntity::class)->findOneBy(['id' => 1]);
-//        var_dump();
 
         $faker = \Faker\Factory::create('sk_SK');
 
@@ -37,23 +35,6 @@ class ArticlesFixture extends \Doctrine\Common\DataFixtures\AbstractFixture
         $manager->persist($articleLang);
 
         $manager->flush();
-
-
-//
-//        $admin = new \Users\User('admin@nette.org');
-//        $admin->setPassword(Passwords::hash('admin'));
-//        $admin->addRole($this->getReference('admin-role'));
-//        $manager->persist($admin);
-//
-//        $demo = new \Users\User('demo@nette.org');
-//        $demo->setPassword(Passwords::hash('demo'));
-//        $demo->addRole($this->getReference('demo-role'));
-//        $manager->persist($demo);
-//
-//        $manager->flush();
-//
-//        $this->addReference('admin-user', $admin);
-//        $this->addReference('demo-user', $demo);
     }
 
 }
