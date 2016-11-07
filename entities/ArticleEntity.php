09a8451f67c9cfb0a92bf12b8c08816f35438c26
shadd0w <rene.gmitter@wame.sk>
2016-11-07 17:28:58 +0100

    FIX ArticleEntity

--- entities/ArticleEntity.php
+++ entities/ArticleEntity.php
@@ -36,24 +36,7 @@ class ArticleEntity extends TranslatableEntity
 	 */
 	protected $publishEndDate;
 	
-	
-	/**
-	 * Add lang
-	 * 
-	 * @param string $lang
-	 * @param object $entity
-	 * @return BaseEntity Created language entity
-	 */
-	public function addLang($lang, $entity = null) {
-		if(!$entity) {
-			$entity = new ArticleLangEntity();
-			$entity->setLang($lang);
-			$entity->setArticle($this);
-		}
-		$this->langs[$lang] = $entity;
-		return $entity;
-	}
-	
+    
 	/** get ************************************************************/
 
 	public function getPublishStartDate()
